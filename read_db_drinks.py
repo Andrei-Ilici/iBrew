d0305831980fb3d3f3e119d0bcf4e1f95fc3faef
Andrei-Ilici <andrei.ilici29@gmail.com>
2020-09-12 19:51:27 +0100

    databases

--- read_db_drinks.py
+++ read_db_drinks.py
@@ -4,14 +4,14 @@ connection = pymysql.connect(
     host =	"localhost",
     port = 3306,
     user = "root",
-    password = "ttii2.",
-    db = "people",
+    password = "ttii2.jI",
+    db = "drinks",
 )
 cursor = connection.cursor()
-cursor.execute("SELECT person_id, first_name FROM person")
+cursor.execute("SELECT drink_id, name, volume FROM drink")
 rows = cursor.fetchall()
 print (rows)
 for row in rows:
-    print("ID - " + str(row[0]) + ", Name - " + row[1])
+    print("ID - " + str(row[0]) + ": " + row[1] + ' has a volume of ' + str(row[2])+' liters.')
 cursor.close()
 connection.close()
---
+++ read_db_people.py
@@ -0,0 +1,17 @@
+import pymysql
+
+connection = pymysql.connect(
+    host =	"localhost",
+    port = 3306,
+    user = "root",
+    password = "ttii2.jI",
+    db = "people",
+)
+cursor = connection.cursor()
+cursor.execute("SELECT person_id, first_name FROM person")
+rows = cursor.fetchall()
+print (rows)
+for row in rows:
+    print("ID - " + str(row[0]) + ", Name - " + row[1])
+cursor.close()
+connection.close()
