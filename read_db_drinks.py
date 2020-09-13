e9385afe64bdd2f8d9daf890a961eb4de7ce5310
Andrei-Ilici <andrei.ilici29@gmail.com>
2020-09-13 21:11:06 +0100

    update

--- read_db_drinks.py
+++ read_db_drinks.py
@@ -11,7 +11,18 @@ cursor = connection.cursor()
 cursor.execute("SELECT drink_id, name, volume FROM drink")
 rows = cursor.fetchall()
 print (rows)
+
 for row in rows:
     print("ID - " + str(row[0]) + ": " + row[1] + ' has a volume of ' + str(row[2])+' liters.')
+
+name = input ("What is the name of your drink? ")
+volume = float(input( "What is the volume of your drink?"))
+
+sql_drink = "INSERT INTO drink (name, volume) VALUES (%s, %s)"
+val_drink = (name, volume)
+
+cursor.execute(sql_drink, val_drink)
+connection.commit()
+
 cursor.close()
 connection.close()
--- read_db_people.py
+++ read_db_people.py
@@ -8,10 +8,21 @@ connection = pymysql.connect(
     db = "people",
 )
 cursor = connection.cursor()
-cursor.execute("SELECT person_id, first_name FROM person")
+cursor.execute("SELECT person_id, first_name, surname, age FROM person")
 rows = cursor.fetchall()
 print (rows)
 for row in rows:
-    print("ID - " + str(row[0]) + ", Name - " + row[1])
+    print("ID - " + str(row[0]) + ", Name - " + row[1] + ' '+ row[2]+ ' is '+ str(row[3]) + ' old.')
+
+first_name = input("The first name is: ")
+surname = input("The surname is: ")
+age = int(input("The age is: "))
+
+sql_person = "INSERT INTO person (first_name, surname, age) VALUES (%s, %s, %s)"
+val_person = (first_name, surname, age)
+
+cursor.execute(sql_person, val_person)
+connection.commit()
+
 cursor.close()
 connection.close()
