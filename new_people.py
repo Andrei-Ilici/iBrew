07a5e0f1d00bc1dc81e7ae969e654937ac960b17
Andrei-Ilici <andrei.ilici29@gmail.com>
2020-09-17 10:52:23 +0100

    3rd button working

--- new_people.py
+++ new_people.py
@@ -1,17 +1,17 @@
 from tkinter import *
+from database import connect
 
 def NewWindow_Addpeople():
+
     new = Tk()
     new.title("Add people")
-    new_name = Entry(new, width=27)
+    new_first_name = Entry(new, width=27)
+    new_surname = Entry(new, width=27)
     new_age = Entry(new, width=27)
-    new_height = Entry(new, width=27)
-    new_fav = Entry(new, width=27)
     
-    new_name.grid(row=1,column=2)
-    new_age.grid(row=2, column=2)
-    new_height.grid(row=3, column=2)
-    new_fav.grid(row=4, column=2)
+    new_first_name.grid(row=1,column=2)
+    new_surname.grid(row=2, column=2)
+    new_age.grid(row=3, column=2)
     
     new_pers_enter = Button(new, text= "Enter", command=lambda: enterval())
     new_pers_enter.grid(row=5, column=2)
@@ -19,19 +19,29 @@ def NewWindow_Addpeople():
     new_pers_exit = Button(new, text="Exit", command=new.destroy)
     new_pers_exit.grid(row=6, column=2)
     
-    name_label = Label(new, text= "Insert name here: ")
+    first_name_label = Label(new, text= "Insert first name here: ")
+    surname_label = Label(new, text= "Insert surname here: ")
     age_label = Label(new, text= "Insert age here: ")
-    height_label = Label(new, text= "Insert height here: ")
-    fav_label = Label(new, text= "   Insert favourite drink here:   ")
 
-    name_label.grid(row=1, column=1)
-    age_label.grid(row=2, column=1)
-    height_label.grid(row=3, column=1)
-    fav_label.grid(row=4, column=1)
+    first_name_label.grid(row=1, column=1)
+    surname_label.grid(row=2, column=1)
+    age_label.grid(row=3, column=1)
     
     def enterval():
-        value_name = new_name.get()
+        connect()
+        from database import connection
+        value_first_name = new_first_name.get()
+        value_surname = new_surname.get()
         value_age = new_age.get()
-        value_height = new_height.get()
-        value_fav = new_fav.get()
-        print(value_name + value_age + value_height + value_fav)    
+        int_age = int(value_age)
+        print(value_first_name + value_surname + str(int_age))
+        
+        cursor_people = connection.cursor()
+
+        sql_person_command = "INSERT INTO person (first_name, last_name, age) VALUES (%s, %s, %s)"
+        val_person = (value_first_name, value_surname, value_age)
+        cursor_people.execute(sql_person_command, val_person)
+        connection.commit()
+        
+        cursor_people.close()
+        connection.close()
